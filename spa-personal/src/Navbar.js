052563df3e44409373820cb47fc4f4c65a93206e
Stephanie Li <listeph15@gmail.com>
2022-06-17 12:47:59 -0500

    added icons and logo but still definitely need adjusting

--- spa-personal/src/Navbar.js
+++ spa-personal/src/Navbar.js
@@ -1,13 +1,19 @@
 import './Navbar.css';
 import logo from './images/sf-logo.png'
+import {CgProfile} from 'react-icons/cg';
+import {FiHelpCircle} from 'react-icons/fi';
 
 
 const Navbar = () => {
     return ( 
         <div>
             <nav className="Navbar">
-                 {/* <img src={logo} alt="State Farm Logo" className="Logo" /> */}
-                <a> Auto Quoting & Purchase On/Off Request</a>
+                <img src={logo} alt="State Farm Logo" className="Logo" />
+                <a className="Title"> Auto Quoting & Purchase On/Off Request</a>
+                <ul className="Links">
+                    <li><a className="Icon" href="#"><FiHelpCircle style={{color: '#403C3C', fontSize:'30px'}}/></a></li>
+                    <li><a className="Icon" href="#"><CgProfile style={{color: '#403C3C', fontSize: '30px'}}/></a></li>
+                </ul>
             </nav>       
         </div>
      );

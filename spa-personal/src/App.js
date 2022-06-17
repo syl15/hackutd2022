dc7dcd35e7f9ceb41fd302c92a29fef79ff64aeb
Stephanie Li <listeph15@gmail.com>
2022-06-17 12:17:22 -0500

    initial commit

---
+++ spa-personal/src/App.js
@@ -0,0 +1,12 @@
+import Navbar
+ from "./Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ spa-personal/src/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css';
+import logo from './images/sf-logo.png'
+
+
+const Navbar = () => {
+    return ( 
+        <div>
+            <nav className="Navbar">
+                 {/* <img src={logo} alt="State Farm Logo" className="Logo" /> */}
+                <a> Auto Quoting & Purchase On/Off Request</a>
+            </nav>       
+        </div>
+     );
+}
+ 
+export default Navbar;
+
